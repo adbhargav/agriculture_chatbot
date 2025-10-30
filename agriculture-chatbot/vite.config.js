d38f006ca18bf4137a24484e2fde31d3a99fa107
BHARGAV A <bhargavandhe202@gmail.com>
2025-10-30 22:28:03 +0530

    Update vite.config.js

--- agriculture-chatbot/vite.config.js
+++ agriculture-chatbot/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If you deploy under a subpath, change base to '/your-subpath/'
+// If you deploy under a subpath, change base to '/your-subpath/
 export default defineConfig({
   base: '/',
   plugins: [react()]
